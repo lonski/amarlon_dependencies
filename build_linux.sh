6fc256a6e434f65913c5b67ce9fba8b1ea4e5988
lonski <michal@lonski.pl>
2015-09-27 11:32:29 +0200

    gcc updating

--- build_linux.sh
+++ build_linux.sh
@@ -12,18 +12,13 @@ NC='\033[0m'
 
 set_gcc()
 {
-	GCC_VERSION=5
-
-	sudo update-alternatives --remove-all gcc
-	sudo update-alternatives --remove-all g++
-
-	sudo apt-get install -qq gcc-$GCC_VERSION g++-$GCC_VERSION
-
-	sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-$GCC_VERSION 20 --slave /usr/bin/g++ g++ /usr/bin/g++-$GCC_VERSION
-	sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
-	sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
-	sudo update-alternatives --set cc /usr/bin/gcc
-	sudo update-alternatives --set c++ /usr/bin/g++
+    sudo apt-get install build-essential
+    sudo apt-get install -qq gcc-5 g++-5
+    sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 20 --slave /usr/bin/g++ g++ /usr/bin/g++-5
+    sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
+    sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 30
+    sudo update-alternatives --set cc /usr/bin/gcc
+    sudo update-alternatives --set c++ /usr/bin/g++
 }
 
 before_build()
