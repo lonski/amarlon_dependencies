2ea20b3136234b9a4de96527cd2eab5a07e87fbd
lonski <michal@lonski.pl>
2015-09-27 08:43:52 +0200

    tcod functions

--- build_linux.sh
+++ build_linux.sh
@@ -1,12 +1,40 @@
-#!/bin/sh
+#!/bin/bash
 
 ROOT_DIR=`pwd`
 LIB_DIR="$ROOT_DIR/lib"
 SRC_DIR="$ROOT_DIR/src"
 
-#TCOD
-cd $SRC_DIR
-cd libtcod
-make -f makefiles/makefile-linux debug
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
+NC='\033[0m'
 
-cp ./libtcod_debug.so $LIB_DIR/libtcod.so
+TCOD_build()
+{
+	cd $SRC_DIR
+	cd libtcod
+
+	echo "${GREEN}--- Building TCOD..${NC}"
+	make -f makefiles/makefile-linux debug
+	echo "${CYAN}--- Copying TCOD libs..${NC}"
+	cp ./libtcod_debug.so $LIB_DIR/libtcod.so
+}
+
+TCOD_clean()
+{
+	cd $SRC_DIR
+	cd libtcod
+
+	echo "${BLUE}--- Cleaning TCOD..${NC}"
+	rm *.so
+	rm lib/libz.a
+	rm $LIB_DIR/libtcod.so
+}
+
+if [ "$1" = "clean" ] 
+then
+	TCOD_clean
+else
+	TCOD_build
+fi
+
